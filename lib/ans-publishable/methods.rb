353f36a936db7343385d192a623b9671c8b7697c
sakai shunsuke <sakai@ans-web.co.jp>
2012-03-28 18:59:04 +0900

    publishable::methods 追加

---
+++ lib/ans-publishable/methods.rb
@@ -0,0 +1,42 @@
+module Ans::Publishable::Methods
+
+  def publish(hash={})
+    publish_id = nil
+
+    retry_limit = 5
+    count = 0
+    begin
+
+      transaction do
+        publish_id = publish_model.create.send(publish_primary_column)
+        where(publish_foreign_column => nil).send(publishable_scope,hash).update_all publish_foreign_column => publish_id
+      end
+
+    rescue => e
+      count += 1
+      if count < retry_limit
+        retry
+      else
+        return []
+      end
+    end
+
+    where(publish_foreign_column => publish_id)
+  end
+
+  private
+
+  def publishable_scope
+    :publishable
+  end
+  def publish_model
+    parent.const_get("#{model_name}Publish")
+  end
+  def publish_foreign_column
+    "#{table_name.singularize}_publish_id".to_sym
+  end
+  def publish_primary_column
+    :id
+  end
+
+end
---
+++ spec/ans-publishable/methods_spec.rb
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+
+require "spec_helper"
+
+describe Ans::Publishable::Methods do
+  include Ans::Feature::Helpers::ActionHelper
+
+  describe ".publish" do
+    before do
+      class AnsPublishableMethods_Article
+        extend Ans::Publishable::Methods
+        def self.transaction
+          yield
+        end
+        def self.table_name
+          "articles"
+        end
+        def self.model_name
+          "AnsPublishableMethods_Article"
+        end
+      end
+      class AnsPublishableMethods_ArticlePublish; end
+      class AnsPublishableMethods_ArticleSend; end
+
+      @id = 1
+      @other_id = 2
+
+      @result = Object.new
+      @other_result = Object.new
+
+      @args = Object.new
+
+      stub(AnsPublishableMethods_Article).where(article_publish_id: nil){AnsPublishableMethods_Article}
+      stub(AnsPublishableMethods_Article).where(send_id: nil){AnsPublishableMethods_Article}
+      stub(AnsPublishableMethods_Article).publishable{AnsPublishableMethods_Article}
+      stub(AnsPublishableMethods_Article).other_publishable{AnsPublishableMethods_Article}
+      stub(AnsPublishableMethods_Article).update_all
+
+      stub(AnsPublishableMethods_Article).where(article_publish_id: @id){@result}
+      stub(AnsPublishableMethods_Article).where(article_publish_id: @other_id){@other_result}
+
+      stub(AnsPublishableMethods_Article).where(send_id: @id){@result}
+      stub(AnsPublishableMethods_Article).where(send_id: @other_id){@other_result}
+
+      the_action do
+        AnsPublishableMethods_Article.publish(@args)
+      end
+    end
+
+    context "例外が発生しない場合" do
+      before do
+        @publish = Object.new
+
+        stub(AnsPublishableMethods_ArticlePublish).create{@publish}
+
+        stub(@publish).send(:id){@id}
+        stub(@publish).send(:other_id){@other_id}
+      end
+
+      context "デフォルトのメソッド構成でコールする場合" do
+        it "は、 Publish を作成してその ID を持つコレクションを返す" do
+          the_action.should == @result
+          AnsPublishableMethods_Article.should have_received.where(article_publish_id: nil)
+          AnsPublishableMethods_Article.should have_received.publishable(@args)
+          AnsPublishableMethods_Article.should have_received.update_all(article_publish_id: @id)
+          AnsPublishableMethods_Article.should have_received.where(article_publish_id: @id)
+        end
+      end
+
+      context "publishable_scope をオーバーライドする場合" do
+        before do
+          stub(AnsPublishableMethods_Article).publishable_scope{:other_publishable}
+        end
+        it "は、 other_publishable を使用して返す" do
+          the_action.should == @result
+          AnsPublishableMethods_Article.should have_received.other_publishable(@args)
+        end
+      end
+
+      context "publish_model をオーバーライドする場合" do
+        before do
+          stub(AnsPublishableMethods_Article).publish_model{AnsPublishableMethods_ArticleSend}
+
+          @other_publish = Object.new
+
+          stub(AnsPublishableMethods_ArticleSend).create{@other_publish}
+
+          stub(@other_publish).send(:id){@id}
+          stub(@other_publish).send(:other_id){@other_id}
+        end
+        it "は、 AnsPublishableMethods_ArticleSend を使用して返す" do
+          the_action.should == @result
+        end
+      end
+
+      context "publish_foreign_column をオーバーライドする場合" do
+        before do
+          stub(AnsPublishableMethods_Article).publish_foreign_column{:send_id}
+        end
+        it "は、 send_id を使用して返す" do
+          the_action.should == @result
+          AnsPublishableMethods_Article.should have_received.where(send_id: nil)
+          AnsPublishableMethods_Article.should have_received.publishable(@args)
+          AnsPublishableMethods_Article.should have_received.update_all(send_id: @id)
+          AnsPublishableMethods_Article.should have_received.where(send_id: @id)
+        end
+      end
+
+      context "publish_primary_column をオーバーライドする場合" do
+        before do
+          stub(AnsPublishableMethods_Article).publish_primary_column{:other_id}
+        end
+        it "は、 other_id を使用して返す" do
+          the_action.should == @other_result
+          AnsPublishableMethods_Article.should have_received.update_all(article_publish_id: @other_id)
+          AnsPublishableMethods_Article.should have_received.where(article_publish_id: @other_id)
+        end
+      end
+
+    end
+
+    context "例外が発生する場合" do
+      before do
+        # retry 5回
+        stub(AnsPublishableMethods_ArticlePublish).create.times(5){raise "作成エラー"}
+      end
+      it "は、空の配列を返す" do
+        the_action.should == []
+      end
+    end
+
+  end
+
+end
+
